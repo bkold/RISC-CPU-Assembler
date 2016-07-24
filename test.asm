476deafba58e1b663d87f8ee86a06d7237cf8e00
bkold <bkold@users.noreply.github.com>
2016-07-24 02:07:46 -0700

    Update test.asm

--- test.asm
+++ test.asm
@@ -5,13 +5,13 @@ BCPUJ_32 Core1_0
 BCPU_32 1, 0
 Sleep_32 30, 0
 BCPUJ_32 Core1_1
+[Main_Loop]
 BCPU_32 1, 1
 
 Add_32 r2, r30, r0 --save r30 into r2
 Add_32 r0, r0, r0  --noop
 Add_32 r0, r0, r0  --noop
 
-[Main_Loop]
 Sleep_32 30, 0
 
 BCPUJR_32 r2
